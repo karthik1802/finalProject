cbcda478b7a13c8c0cc168b083cbd51bc7ae4f5d
akay <akay1802@gmail.com>
2019-05-07 07:09:13 +0530

    Final Commit(hopefully)

--- app/routes.py
+++ app/routes.py
@@ -13,11 +13,14 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, roc_curve, auc
 from sklearn.neural_network import MLPClassifier
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 from keras.models import Sequential
 from keras.layers import Dense
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+
 
 @app.route('/')
 @app.route('/index')
@@ -28,8 +31,8 @@ def index():
 
 
 
-@app.route('/bayseian', methods = ['GET', 'POST'])
-def bayseianClassifier():
+@app.route('/bayesian', methods = ['GET', 'POST'])
+def bayesian():
     
     data = pd.read_csv("app/oasis_longitudinal.csv")
     print(data.head())
@@ -112,62 +115,94 @@ def svm():
 
 @app.route('/ann')
 def ann():
-    dataset = pd.read_csv("app/oasis_longitudinal.csv")
+    data = pd.read_csv("app/oasis_longitudinal.csv")
+    print(data.head())
+    # Convert categorical variable to numeric
+    data["M/F_C"] = np.where(data["M/F"] == "M", 0, 1)
+    data["Group_C"] = np.where(data["Group"] == "Demented", 0, 1)
+    # Cleaning dataset of NaN
+    data = data[['M/F_C', 'Age', 'EDUC', 'SES', 'MMSE', 'eTIV',
+                 'nWBV', 'ASF', 'Group_C','CDR']].dropna(axis=0, how='any')
 
-
-    dataset['SES'].fillna((dataset['SES'].median()), inplace=True)
-    dataset['MMSE'].fillna((dataset['MMSE'].median()), inplace=True)
-    dataset['CDR'].fillna((dataset['CDR'].mean()), inplace=True)
-    dataset['eTIV'].fillna((dataset['eTIV'].mean()), inplace=True)
-    dataset['nWBV'].fillna((dataset['nWBV'].mean()), inplace=True)
-    dataset['ASF'].fillna((dataset['ASF'].mean()), inplace=True)
+   
     used_features = ['CDR', 'SES', 'MMSE', 'eTIV', 'nWBV', 'ASF']
 
-    x = dataset.iloc[:, 4:15].values
-    y = dataset.iloc[:, 2:3].values
-    labelencoder_X_1=LabelEncoder()
-    x[:,1]=labelencoder_X_1.fit_transform(x[:,1])
-    labelencoder_X_2=LabelEncoder()
-    x[:,2]=labelencoder_X_2.fit_transform(x[:,2])
-    onehotencoder=OneHotEncoder(categorical_features=[1])
-    x=onehotencoder.fit_transform(x).toarray()
-    x=x[:,1:]
-    labelencoder_y_1=LabelEncoder()
-    onehotencoder1=OneHotEncoder(categorical_features=[0])
-    y[:,0]=labelencoder_y_1.fit_transform(y[:,0])
-    y=onehotencoder1.fit_transform(y).toarray()
-    X_train, X_test, y_train, y_test = train_test_split(
-    x,y,test_size=0.25, random_state=0)
     
-    sc=StandardScaler()
-    x_train=sc.fit_transform(X_train)
-    x_test=sc.transform(X_test)
-    classifier=Sequential()
-    classifier.add(Dense(output_dim=6,init='uniform',activation='relu',input_dim=11))
-    classifier.add(Dense(output_dim=6,init='uniform',activation='relu'))
-    classifier.add(Dense(output_dim=3,init='uniform',activation='sigmoid'))
-    classifier.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
-    classifier.fit(x_train,y_train,batch_size=10,nb_epoch=100)
+    X_train, X_test = train_test_split(data,test_size=0.5, random_state=0)
+    
+    # sc=StandardScaler()
+    # x_train=sc.fit_transform(X_train)
+    # x_test=sc.transform(X_test)
 
-    y_pred=classifier.predict(x_test)
+    # I already found out the tuned hyper parameters so commenting the code.
+
+   
+    
+
+    # def build_classifier(optimizer):
+    #     classifier = Sequential()
+    #     classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 6))
+    #     classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+    #     classifier.add(Dense(units = 3, kernel_initializer = 'uniform', activation = 'relu'))
+    #     classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+    #     classifier.compile(optimizer = optimizer, loss = 'binary_crossentropy', metrics = ['accuracy'])
+    #     return classifier
+    # classifier = KerasClassifier(build_fn = build_classifier)
+    # parameters = {'batch_size': [1, 5],
+    #             'epochs': [100, 120],
+    #             'optimizer': ['adam', 'rmsprop']}
+    # grid_search = GridSearchCV(estimator = classifier,
+    #                         param_grid = parameters,
+    #                         scoring = 'accuracy',
+    #                         cv = 10)
+    # grid_search = grid_search.fit(X_train[used_features].values, X_train['Group_C'])
+
+
+    # best_parameters = grid_search.best_params_
+    # best_accuracy = grid_search.best_score_
+    # print("best_parameters: ")
+    # print(best_parameters)
+    # print("\nbest_accuracy: ")
+    # print(best_accuracy)
+    # return("Done!!")
+
+    #####RESULTS######
+    # best_parameters: {'batch_size': 1, 'epochs': 100, 'optimizer': 'rmsprop'}
+
+    # best_accuracy: 0.978021978022
+
+
+    classifier = Sequential() # Initialising the ANN
+
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 6))
+    classifier.add(Dense(units = 3, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = 2, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+
+    classifier.compile(optimizer = 'rmsprop', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+    classifier.fit(X_train[used_features].values, X_train["Group_C"], batch_size = 1, epochs = 100)
+
+    y_pred=classifier.predict(X_test[used_features])
     # classifier.fit(x_test,y_test,batch_size=2,nb_epoch=100)
     tableData = []
     i = 0
-    # print(y_pred,x_test)
-    new_prediction=classifier.predict(sc.transform(np.array([[0.0,0,600,1,40,3,60000,2,1,1,50000]])))
-    print(new_prediction)
-    # for index, row in X_test.iterrows():
-    #     dic = dict(row)
-    #     print(row[0])
-    #     dic["Group"] = "Affected" if dic["Group_C"] else "Not Affected"
-    #     dic["Prediction"] = "Affected" if y_pred[i] else "Not Affected"
-    #     dic["Gender"] = "Male" if dic["M/F_C"] else "Female"
-    #     dic["green"] = True if (dic['Group'] == dic["Prediction"]) else False
-    #     print(dic["green"])
-    #     i = i+1
-    #     tableData.append(dic)
-    return("Done")
-    return render_template("table.html", data=tableData, green=dic["green"], model="Artificial Neural Network", accur=100*(1-(X_test["Group_C"] != y_pred).sum()/X_test.shape[0]))
+    print(y_pred)
+    tr=0
+    for index, row in X_test.iterrows():
+        dic = dict(row)
+        print(row[0])
+        dic["Group"] = "Affected" if dic["Group_C"] else "Not Affected"
+        dic["Prediction"] = "Affected" if y_pred[i]>=0.5 else "Not Affected"
+        dic["Gender"] = "Male" if dic["M/F_C"] else "Female"
+        dic["green"] = True if (dic['Group'] == dic["Prediction"]) else False
+        print(dic["green"])
+        if dic["Group"] == dic["Prediction"]:
+            tr=tr+1
+        i = i+1
+        tableData.append(dic)
+    # return("Done")
+    return render_template("table.html", data=tableData, green=dic["green"], model="Artificial Neural Network", accur=100*(tr/i))
 
 
 @app.route('/table', methods = ['GET', 'POST'])
@@ -182,9 +217,9 @@ def upload_file():
     if request.method == 'POST':
         f = request.files['file']
         f.save(f.filename)
-        return 'file uploaded successfully'+f.filename
+        return redirect(url_for('cat'))
     #return render_template('upload.html')
 
 @app.route('/cat', methods = ['GET', 'POST'])
-def showCat():
+def cat():
     return render_template("category.html")
